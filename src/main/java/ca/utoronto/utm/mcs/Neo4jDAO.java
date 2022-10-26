9fb15f542d24e3cca39b2b13264695a5e6744a80
BillyZ1435 <billi.zhou@mail.utoronto.ca>
2022-10-25 22:28:42 -0400

    Incorporated tut code

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
+++ src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
@@ -1,7 +1,28 @@
 package ca.utoronto.utm.mcs;
 
+import org.neo4j.driver.*;
+
 // All your database transactions or queries should 
 // go in this class
 public class Neo4jDAO {
-    // TODO Complete This Class
+    private final Session session;
+    private final Driver driver;
+
+    private final String uriDb = "bolt://localhost:7687";
+    private final String username = "neo4j";
+    private final String password = "123456";
+
+
+    public Neo4jDAO() {
+        this.driver = GraphDatabase.driver(this.uriDb, AuthTokens.basic(this.username, this.password));
+        this.session = this.driver.session();
+    }
+
+    public void addActor(String name, String actorId) {
+        String query;
+        query = "CREATE (a:actor {name: \"%s\", actorId: \"%s\"})";
+        query = String.format(query, name, actorId);
+        this.session.run(query);
+        return;
+    }
 }
--- src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
+++ src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
@@ -3,13 +3,63 @@ package ca.utoronto.utm.mcs;
 import java.io.IOException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONException;
+import org.json.*;
 
 public class ReqHandler implements HttpHandler {
 
     // TODO Complete This Class
 
+    public Neo4jDAO dao;
+
+    public ReqHandler() {
+        this.dao = new Neo4jDAO();
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        
+        try {
+            switch (exchange.getRequestMethod()) {
+                case "GET":
+                    //this.handleGet(exchange);
+                    break;
+                case "POST":
+                    this.handlePost(exchange);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handlePost(HttpExchange r) throws IOException, JSONException {
+        String body = Utils.convert(r.getRequestBody());
+        try {
+            JSONObject deserialized = new JSONObject(body);
+
+            String name, actorId;
+
+            if (deserialized.length() == 2 && deserialized.has("name") && deserialized.has("actorId")) {
+                name = deserialized.getString("name");
+                actorId = deserialized.getString("actorId");
+            } else {
+                r.sendResponseHeaders(400, -1);
+                return;
+            }
+
+            try {
+                this.dao.addActor(name, actorId);
+            } catch (Exception e) {
+                r.sendResponseHeaders(500, -1);
+                e.printStackTrace();
+                return;
+            }
+            r.sendResponseHeaders(200, -1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            r.sendResponseHeaders(500, -1);
+        }
     }
 }
